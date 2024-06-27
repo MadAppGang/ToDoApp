3e329ff02893eda72f2979cf0abb4304034ce919
Jack Rudenko <mail@erudenko.com>
2024-06-27 21:19:32 +1000

    add cors

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,11 @@ import { Hono } from "hono";
 import { logger } from "hono/logger";
 import authRouter from "./auth_router.ts";
 import todoRouter from "./todo_router.ts";
+import { cors } from 'hono/cors'
 
 const app = new Hono({strict: false});
 app.use("*", logger());
+app.use("*", cors())
 
 app.get("/test", (c) => {
   return c.json({ message: "Test passed" });
